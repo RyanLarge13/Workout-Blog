08ca8dffd18a786d52486289b1adcfcebc95648f
RyanLarge13 <ryanlarge13@gmail.com>
2023-02-09 00:00:57 -0800

    Redefining UI after Schema change
    I love everything about this commit. The progress made after changing the Schema for Sanity and the cool new implimentations
    of data made this application two times as fun!
    
    Now a user straight from his dashboard will be able to view blog posts, search blog posts by title with live typing!! A post now shows
    the user who created it and will soon have likes and comments attached to it as well.
    
    These implementations were crutial to a fun website creation. Next will be viewing a single post created and the content within it.
    This will include titles, dates, comments, likes, sharing, descriptions, more photos, and the body.

--- backend/schemas/category.js
+++ backend/schemas/category.js
@@ -1,17 +1,22 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Category",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "url",
     },
   ],
-}
+};
