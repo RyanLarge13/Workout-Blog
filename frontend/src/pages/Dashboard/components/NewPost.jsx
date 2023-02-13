60bc2c665c1b8c09982edaa8cab4689ef8597a26
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-02-12 16:32:21 -0800

    Deleting CKEditor
    Because of the lack of ease integrating an image picker with CKEditor I choose to instead uninstall their
    react package and intergrate a new rich text editor with ease of image uploading & url uploading.
    
    CKEditor with images requires you to create multiple accounts with them and subscriptions if functionality is to
    be of a decent standard which defeats the purpose of this blog and how I am writting the code for it.
    
    As my first you tube video, I plan to walk people through creating this website and plan for it to be of the uttmost ease
    for them.

--- frontend/src/pages/Dashboard/components/NewPost.jsx
+++ frontend/src/pages/Dashboard/components/NewPost.jsx
@@ -1,26 +1,7 @@
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-
 const NewPost = () => {
   return (
     <section>
-      <CKEditor
-        editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={(editor) => {
-          console.log("Editor is ready to use!", editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log("Blur.", editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log("Focus.", editor);
-        }}
-      />
+      <h1>New Post</h1>
     </section>
   );
 };
